5155f3450b77c13267a350150641318484229041
Devansh <devcodes21@gmail.com>
2025-04-27 00:36:13 +0530

    Add default values for created_at and updated_at timestamps

--- venueservices/models.py
+++ venueservices/models.py
@@ -13,8 +13,8 @@ class Booking(models.Model):
     waiter = models.ForeignKey(Waiter, on_delete=models.SET_NULL, null=True, blank=True, related_name='bookings')
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='bookings')
     total_bill = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, default = timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, default = timezone.now)
 
     def __str__(self):
         return f"Booking {self.booking_id} at {self.venue.name}, Table {self.table.table_number}"
@@ -24,8 +24,8 @@ class Cart(models.Model):
     cart_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='cart')
     total_bill = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, default = timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, default = timezone.now)
 
     def __str__(self):
         return f"Cart {self.cart_id} for Booking {self.booking.booking_id}"
@@ -42,6 +42,5 @@ class CartItem(models.Model):
         return f"CartItem {self.cart_item_id} for Cart {self.cart.cart_id}"
 
     def save(self, *args, **kwargs):
-        # Calculate total price as price * quantity before saving
         self.total_price = self.menu_item.price * self.quantity
         super().save(*args, **kwargs)
