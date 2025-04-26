655c8bcc088090b635d67fefd86d7675f74f4e03
Devansh <devcodes21@gmail.com>
2025-04-27 03:20:34 +0530

    Change to uuid field

--- authentication/models.py
+++ authentication/models.py
@@ -78,7 +78,7 @@ class Waiter(models.Model):
     managers = models.ManyToManyField(Manager, related_name='waiters')
 
 class RequestedOwner(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     STATUS_CHOICES = [
         ('pending', 'Pending'),
         ('accepted', 'Accepted'),
