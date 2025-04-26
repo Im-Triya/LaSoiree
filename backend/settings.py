8408e8fa4201a513b401d94d77c1ed1078d3f0b0
Devansh <devcodes21@gmail.com>
2025-04-27 02:32:58 +0530

    Adding AutoField

--- backend/settings.py
+++ backend/settings.py
@@ -162,6 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
