fa5bd3c6c6b9d9b61d35500ab328356293ccccab
Triya <triyaroy0401@gmail.com>
2025-05-18 22:54:25 +0530

    creating chat from scratch

--- chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('private-chat/', PrivateChatView.as_view(), name='private-chat'),
-    path('active-users/<int:res_id>/', ActiveChatUsersView.as_view(), name='active-chat-users'),
-    path('mark-inactive/', MarkUserInactiveView.as_view(), name='mark-user-inactive'),
-]
