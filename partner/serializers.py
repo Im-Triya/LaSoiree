b80dab68b05e38d0bff9064bb4360c1a9ed0fc13
Triya <triyaroy0401@gmail.com>
2025-06-26 16:47:49 +0530

    Updated API to return all data for get menu API

--- partner/serializers.py
+++ partner/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Venue, Table, Menu, Offer
+from decimal import Decimal, InvalidOperation
 
 
 class VenueSerializer(serializers.ModelSerializer):
